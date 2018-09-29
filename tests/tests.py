aa97e60ea50b622cf30ce3e69ee14fc17be88eea
Mizuki Sato <mizki.sato.0025@gmail.com>
2018-09-29 14:29:28 +0900

    いろんな飲みものかえた

--- tests/tests.py
+++ tests/tests.py
@@ -5,11 +5,34 @@ class VendingMachine:
     def __init__(self):
         self.receipt_amount = 0
 
+    def cola(self):
+        if self.receipt_amount >= 100:
+            return 'コーラ'
+        else:
+            return None
+
+    def oolong(self):
+        if self.receipt_amount >= 100:
+            return 'ウーロン茶'
+        else:
+            return None
+        
+    def redbull(self):
+        if self.receipt_amount >= 200:
+            return 'レッドブル'
+        else:
+            return None
+
+
     def click(self, beverage=None):
         if beverage is None:
             return None
-        elif self.receipt_amount >= 100:
-            return beverage
+        elif beverage ==  "コーラ":
+            return self.cola()
+        elif beverage ==  "ウーロン茶":
+            return self.oolong()
+        elif beverage ==  "レッドブル":
+            return self.redbull()                 
         else:
             return None
     
