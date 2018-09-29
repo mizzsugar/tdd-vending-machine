670e17742fd21357114cb077eaa15132ad25efdb
Mizuki Sato <mizki.sato.0025@gmail.com>
2018-09-29 14:14:38 +0900

    100円入れたらコーラ出た

--- tests/tests.py
+++ tests/tests.py
@@ -2,8 +2,17 @@ import pytest
 import unittest
 
 class VendingMachine:
+    def __init__(self):
+        self.receipt_amount = 0
+
     def click(self):
-        return "コーラ"
+        if self.receipt_amount >= 100:
+            return "コーラ"
+        else:
+            return None
+    
+    def insert(self, coin: int):
+        self.receipt_amount += coin
 
 class TestVendingMachine(unittest.TestCase):
     def test_ボタンを押すとコーラが出る(self):
@@ -12,7 +21,7 @@ class TestVendingMachine(unittest.TestCase):
         assert vending_machine.click() == None
 
         vending_machine.insert(100)
-        assert vending_machine.click() == 'コーラ'
+        assert vending_machine.click('コーラ') == 'コーラ'
 
 
 if __name__ == "__main__":
